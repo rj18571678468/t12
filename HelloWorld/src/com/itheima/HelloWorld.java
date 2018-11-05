c44a3a98964bacbe737d5d4a367fc7ed8c68ecad
rongjie <626989279@qq.com>
2018-11-05 19:44:32 +0800

    创建了一个工程

---
+++ HelloWorld/src/com/itheima/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.itheima;/**
+ * Created by tom_plus on 2018年11月05日
+ */
+
+/**
+ * @ProjectName: HelloWorld
+ * @Package: com.itheima
+ * @ClassName: HelloWorld
+ * @Author: Administrator
+ * @Description: ${description}
+ * @Date: 2018/11/5 0005 19:26
+ * @Version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
